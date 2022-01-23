e5cf8d7c3ebd7b69f43e410441b36ec41612babe
Indracit S <indrajithij802@gmail.com>
2022-01-23 19:44:51 +0530

    First commit

--- myApp.js
+++ myApp.js
@@ -4,5 +4,5 @@ console.log('Hello World')
 app.use('/public', express.static(__dirname + '/public'))
 var absolutePath = __dirname + '/views/index.html'
 app.get('/', (req, res) => res.sendFile(absolutePath))
-
+app.get('/json', (req, res) => res.json({ message: 'Hello json' }))
 module.exports = app
