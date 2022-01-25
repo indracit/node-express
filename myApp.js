5b60dc530d9cf51f40fd1aa1c96a9090835c0878
Indracit S <indrajithij802@gmail.com>
2022-01-25 14:14:44 +0530

    First commit

--- myApp.js
+++ myApp.js
@@ -1,8 +1,18 @@
+require('dotenv').config()
 var express = require('express')
 var app = express()
 console.log('Hello World')
 app.use('/public', express.static(__dirname + '/public'))
 var absolutePath = __dirname + '/views/index.html'
+console.log(process.env.MESSAGE_STYLE)
+
 app.get('/', (req, res) => res.sendFile(absolutePath))
-app.get('/json', (req, res) => res.json({ message: 'Hello json' }))
+
+app.get('/json', (req, res) => {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({ message: 'HELLO JSON' })
+  } else {
+    res.json({ message: 'Hello json' })
+  }
+})
 module.exports = app
