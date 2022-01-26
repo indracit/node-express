347c145e7b191bcedabccacb75e57ffbb7dc4669
Indracit S <indrajithij802@gmail.com>
2022-01-26 09:34:14 +0530

    First commit

--- myApp.js
+++ myApp.js
@@ -3,8 +3,11 @@ var express = require('express')
 var app = express()
 console.log('Hello World')
 app.use('/public', express.static(__dirname + '/public'))
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+  next()
+})
 var absolutePath = __dirname + '/views/index.html'
-console.log(process.env.MESSAGE_STYLE)
 
 app.get('/', (req, res) => res.sendFile(absolutePath))
 
