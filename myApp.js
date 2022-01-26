9c5300942170bbf200f5eeb6cbfb379511442995
Indracit S <indrajithij802@gmail.com>
2022-01-26 10:26:24 +0530

    First commit

--- myApp.js
+++ myApp.js
@@ -18,4 +18,15 @@ app.get('/json', (req, res) => {
     res.json({ message: 'Hello json' })
   }
 })
+
+app.get(
+  '/now',
+  (req, res, next) => {
+    req.time = new Date().toString()
+    next()
+  },
+  (req, res) => {
+    res.json({ time: req.time })
+  }
+)
 module.exports = app
